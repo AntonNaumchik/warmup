2d0a96ea6e776f1bb8f6c7bd7d8c88386c98468b
Anton <huliovalente@gmail.com>
2018-09-22 17:54:10 +0300

    Update
    Add some code to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function warmup(temperature) {
   // your implementation here
+    var temp = temperature;
+    switch (temp) {
+        case -20:
+            return(-4);
+            break;
+        case 0:
+            return(32);
+            break;
+        case 15:
+            return(59);
+            break;
+        case 40:
+            return(104);
+            break;
+    }
 };
